eb75a2d0dbaadcf1158f180077eb359aeb3eb80a
alsemol <alsemol@alumni.uv.es>
2023-07-18 10:53:03 +0300

    Modify server to keep track of conversation and count tokens

--- src/Server.js
+++ src/Server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Configuration, OpenAIApi } from "openai";
 import cors from "cors";
 import dotenv from "dotenv";
+import { countTokensInConversation } from "./Utils/OpenAIUtils.js";
 
 dotenv.config();
 const app = express();
@@ -12,19 +13,28 @@ const openai = new OpenAIApi(
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+let conversation = [
+  { role: "system", content: "You are a helpful assistant." },
+];
+
 app.post("/api/generate-text", async (req, res) => {
-  const messages = req.body.prompt;
+  const userMessage = req.body.prompt;
+  conversation.push({ role: "user", content: userMessage });
 
   try {
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: "You are a helpful assistant." },
-        { role: "user", content: messages },
-      ],
+      messages: conversation,
     });
 
-    res.json(response.data.choices[0].message.content);
+    const assistantMessage = response.data.choices[0].message.content;
+    conversation.push({ role: "assistant", content: assistantMessage });
+    res.json(assistantMessage);
+    console.log(
+      `Total tokens used in conversation: ${countTokensInConversation(
+        conversation
+      )}`
+    );
   } catch (err) {
     console.error(err.response.data.error);
     res.status(500).json({ error: "An error occurred" });
--- src/Utils/OpenAIUtils.js
+++ src/Utils/OpenAIUtils.js
@@ -1,17 +1,16 @@
-import { WordTokenizer } from 'natural';
-const tokenizer = new WordTokenizer();
+import { encode } from "gpt-3-encoder";
 
 /**
  * Calculates the number of tokens in a conversation.
- * Tokens are defined as words or meaningful units of text, and are identified using a tokenizer from the `natural` library.
+ * Tokens are defined as words or meaningful units of text, and are identified using the `gpt-3-encoder` library.
  * @param {Array} conversation - The conversation messages. Each message is an object with 'role' and 'content' properties.
  * @returns {Number} - The total number of tokens in the conversation.
  */
-function countTokensInConversation(conversation) {
-    return conversation.reduce((totalTokens, message) => {
-        // Use the tokenizer to split the message content into tokens
-        const tokens = tokenizer.tokenize(message.content);
-        // Add the number of tokens in this message to the total count
-        return totalTokens + tokens.length;
-    }, 0); // Start counting from 0
+export function countTokensInConversation(conversation) {
+  return conversation.reduce((totalTokens, message) => {
+    // Use gpt-3-encoder's encode function to split the message content into tokens
+    const tokens = encode(message.content);
+    // Add the number of tokens in this message to the total count
+    return totalTokens + tokens.length;
+  }, 0);
 }
