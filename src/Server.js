4363e445528694549cc5f1559420177985526a2f
alsemol <alsemol@alumni.uv.es>
2023-07-18 10:09:58 +0300

    Update imports in server file to use ES modules

--- src/Server.js
+++ src/Server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const { Configuration, OpenAIApi } = require("openai");
-const cors = require("cors");
-const process = require("dotenv").config();
+import express from "express";
+import { Configuration, OpenAIApi } from "openai";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const openai = new OpenAIApi(
-  new Configuration({ apiKey: process.parsed.API_KEY })
+  new Configuration({ apiKey: process.env.API_KEY })
 );
 
 app.use(cors({ origin: "*" }));
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.post("/api/generate-text", async (req, res) => {
   const messages = req.body.prompt;
+
   try {
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
